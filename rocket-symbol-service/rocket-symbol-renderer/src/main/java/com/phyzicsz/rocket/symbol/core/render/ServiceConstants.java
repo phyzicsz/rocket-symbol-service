9f4068c6cf0d9483f5628eadacd8e349ead857b6
phyzicsz <phyzics.z@gmail.com>
2020-08-22 14:17:51 -0500

    refactor

---
+++ rocket-symbol-service/rocket-symbol-renderer/src/main/java/com/phyzicsz/rocket/symbol/core/render/ServiceConstants.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2020 phyzicsz <phyzics.z@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.phyzicsz.rocket.symbol.core.render;
+
+/**
+ *
+ * @author phyzicsz <phyzics.z@gmail.com>
+ */
+public class ServiceConstants {
+    public static final String COLOR = "SymbolServiceProperties.Color";
+    
+    public static final String IMAGE_SIZE = "SymbolServiceProperties.Size";
+    
+    public static final Integer DEFAULT_IMAGE_SIZE = 128;
+}
